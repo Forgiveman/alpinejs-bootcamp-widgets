4498476bc487a7c067e18098ec55ed8451fbb0db
Forgiveman <fkmmcmola19@gmail.com>
2022-07-06 11:33:19 +0200

    added files

---
+++ transport-fee.js
@@ -0,0 +1,17 @@
+function transportFee(shift) {
+    if (!shift) {
+      return "";
+    }
+    if (shift.startsWith("morning")) {
+      return "R20";
+    }
+    else if (shift.startsWith("afternoon") || shift.startsWith("day")) {
+      return "R10";
+    }
+    else if (shift.startsWith("night")) {
+      return "FREE";
+    }
+    else {
+      return "";
+    }
+  }
